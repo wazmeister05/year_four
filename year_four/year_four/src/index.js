b9f23faa6f732b7b994578538f3991d19c9c80fc
GA <w.stolton@protonmail.com>
2022-02-04 21:15:57 +0000

    Login setup done

---
+++ year_four/year_four/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './Javascript/Main';
+import './css/stylesheet.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Main/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
